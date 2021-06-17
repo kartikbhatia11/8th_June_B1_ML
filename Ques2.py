de28327816fd4404a4b1a2e9b885d12c70233fb7
kartikbhatia11 <85737145+kartikbhatia11@users.noreply.github.com>
2021-06-17 10:24:47 +0530

    Add files via upload

---
+++ Ques2.py
@@ -0,0 +1,44 @@
+Python 3.9.5 (tags/v3.9.5:0a7dcbd, May  3 2021, 17:27:52) [MSC v.1928 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 5**9
+1953125
+>>> 3//2
+1
+>>> 7//3
+2
+>>> 7/3
+2.3333333333333335
+>>> 6 == 6
+True
+>>> a = 20
+>>> a += 30
+>>> a %= 3;
+>>> print(a)
+2
+>>> True * False
+0
+>>> True & False
+False
+>>> True and False
+
+False
+>>> ((6>3) and (7<4) or (18==3)) and (9>3)
+
+False
+>>> True is False
+
+False
+>>> False in ‘False’
+
+SyntaxError: invalid character '‘' (U+2018)
+>>> False in 'False'
+Traceback (most recent call last):
+  File "<pyshell#15>", line 1, in <module>
+    False in 'False'
+TypeError: 'in <string>' requires string as left operand, not bool
+>>> 'False' in 'False'
+True
+>>> ((True == False) or (False > True)) and (False <= True)
+
+False
+>>> 
